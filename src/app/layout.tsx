b03a869eabd9edeb7c1fb7048ad56f6f4595d6fc
Caio Vellani <caiovellani@outlook.com>
2026-03-13 10:11:20 -0300

    chore: add eslint sort and prettier tailwind plugin

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,25 +1,25 @@
-import type { Metadata } from 'next'
-import { Poppins } from 'next/font/google'
-import './globals.css'
+import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
+import "./globals.css";
 
 const poppins = Poppins({
-  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
-  subsets: ['latin'],
-})
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  subsets: ["latin"],
+});
 
 export const metadata: Metadata = {
-  title: 'Food Delivery',
-  description: 'A food delivery app',
-}
+  title: "Food Delivery",
+  description: "A food delivery app",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
       <body className={`${poppins.className} antialiased`}>{children}</body>
     </html>
-  )
+  );
 }
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,7 @@
 export default function ProductPage() {
   return (
-    <div className="p-5 border border-red-500 rounded-xl">
+    <div className="rounded-xl border border-red-500 p-5">
       <h1>Product Page</h1>
     </div>
-  )
+  );
 }
